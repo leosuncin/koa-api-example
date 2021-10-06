e0e0babf0fbeec77d9f9c45c075a2bca4c542195
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-10-06 11:02:14 -0600

    feat: ✨ setup typeorm with koa

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
+import 'reflect-metadata';
+
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import logger from "koa-logger";
+import { createConnection } from 'typeorm';
 
 import env from "@/config/environment";
 import rootRoutes from "@/routes/root";
@@ -16,9 +19,15 @@ server.use(logger());
 server.use(rootRoutes.routes()).use(rootRoutes.allowedMethods());
 
 if (require.main === module) {
+  createConnection().then(() =>
   server.listen(env.PORT, () => {
     console.info(`Listening at http://localhost:${env.PORT}`);
-  });
+  })
+  ).catch(error => setImmediate(() => {
+    console.error('Unable to run the server because of the following error:');
+    console.error(error);
+    process.exitCode = 1;
+  }))
 }
 
 export default server;
